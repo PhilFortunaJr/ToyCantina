f44bb7ff70c88c84289cf3f8d976251fd45548da
PhilFortunaJr <philfortunajr@gmail.com>
2019-01-16 13:07:23 -0500

    3.10 1st commit

---
+++ app/controllers/simple_pages_controller.rb
@@ -0,0 +1,10 @@
+class SimplePagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'simple_pages/index'
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  root 'simple_pages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
+
+
+# redo simple pages index
+# note some of it is done already so localhost will not work
+# be careful what gets deleted
+# can go on nameofapp to reset app.html.erb and this page
+# May the force be with you
