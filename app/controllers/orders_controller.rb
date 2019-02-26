ef7acea103226ac154ef483cbb01f910fea659b6
PhilFortunaJr <philfortunajr@gmail.com>
2019-02-26 13:12:38 -0500

    4.6 orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,14 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders = Order.includes(:product).all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
   end
 
-  def create
-  end
-
-  def destroy
-  end
 end
