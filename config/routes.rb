e09916514a8a34fcda8193c4f6d146bbb31c1e72
PhilFortunaJr <philfortunajr@gmail.com>
2019-02-27 20:26:00 -0500

    fixed carousel

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/orders'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
