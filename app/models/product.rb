271f6c4341ab40dd479cebc76c27d7cb871b6ed0
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-02 10:50:49 -0500

    5.2 working search bar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  def self.search(search_term)
+  if Rails.env.production?
+    Product.where("name ilike ?", "%#{search_term}%")
+  else
+    Product.where("name LIKE ?", "%#{search_term}%")
+  end
+end
 end
