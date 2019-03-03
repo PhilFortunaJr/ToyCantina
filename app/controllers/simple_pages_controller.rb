7680f596596eaa94c96d69520e1118df68059303
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-03 11:46:35 -0500

    5.3 contact, thank_you, sendgrid

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -14,4 +14,14 @@ class SimplePagesController < ApplicationController
   def landing_page
      @products = Product.limit(3)
   end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'philfortunajr@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/orders'
+  get 'simple_pages/landing_page'
   root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'simple_pages/thank_you'
 end
-
-
 
 
 # redo simple pages index
