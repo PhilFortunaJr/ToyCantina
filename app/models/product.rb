78de5922c93d3555de8193e97e5a6cadd433c81d
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-08 15:27:25 -0500

    5.7 comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,22 @@
 class Product < ApplicationRecord
+
+  has_many :orders
+  has_many :comments
+
   def self.search(search_term)
   if Rails.env.production?
     Product.where("name ilike ?", "%#{search_term}%")
   else
     Product.where("name LIKE ?", "%#{search_term}%")
   end
-end
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_desc.last
+  end
+
 end
