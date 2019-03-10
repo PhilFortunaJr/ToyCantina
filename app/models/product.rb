e6582a15016131de16951159a46c92f5ad9102c4
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-10 12:39:47 -0400

    5.8 comment controller and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,10 @@ class Product < ApplicationRecord
   end
   end
 
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
   def highest_rating_comment
     comments.rating_desc.first
   end
