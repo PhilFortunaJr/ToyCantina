a852525b458a7e2b8f45efe3e736ec84f6ea8719
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-18 15:40:23 -0400

    5.11 email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,15 @@ class UserMailer < ApplicationMailer
   default from: "thetoycantina@gmail.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(from: email,
-         to: 'foxxdocter@gmail.com',
-         subject: "A new contact form message from #{name}")
+      to: 'foxxdocter@gmail.com',
+      subject: "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
+    @appname = "Toy Cantina"
+    mail(to: user.email,
+         subject: "Welcome to #{@appname}!")
   end
 end
