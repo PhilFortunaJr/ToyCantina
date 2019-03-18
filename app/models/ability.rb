92c89f2ceb6409256a8cb41c713663ed7b2c5e8f
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-18 06:50:49 -0400

    5.10 admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can [:show, :edit, :update], User, id: user.id
+      can [:show, :index], Order, user_id: user.id
+      can :create, Comment, user_id: user.id
+      can [:show, :index], Product
+    end
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
