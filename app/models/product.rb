39c3bb38d43ad85b743bda18e7d0dce3b2345c70
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-18 18:46:11 -0400

    5.11 styling changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,12 +5,14 @@ class Product < ApplicationRecord
   validates :name, presence: true
 
   def self.search(search_term)
-  if Rails.env.production?
-    Product.where("name ilike ?", "%#{search_term}%")
-  else
-    Product.where("name LIKE ?", "%#{search_term}%")
+    search_verb = Rails.env.production? ? 'ilike' : 'LIKE'
+    Product.where("name #{search_verb} ? OR description #{search_verb}", "%#{search_term}%")
   end
-  end
+
+  # if Rails.env.production?
+    # Product.where("name ilike ? OR description ilike ?", "%#{search_term}%", "%#{search_term}%")
+  # else
+    # Product.where("name LIKE ? OR description ilike ?", "%#{search_term}%", "%#{search_term}%")
 
   def highest_rating_comment
     comments.rating_desc.first
