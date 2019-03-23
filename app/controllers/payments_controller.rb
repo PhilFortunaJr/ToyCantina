da94edc4359220dab924963c6d4a83f9ec0996d3
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-22 21:18:00 -0400

    6.6.3

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -30,4 +30,5 @@ class PaymentsController < ApplicationController
             flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
           end
         redirect_to product_path(@product)
+  end
 end
