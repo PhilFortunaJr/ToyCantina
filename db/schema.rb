e9aac6ab0a9a83450bcd9eb7faceb15566c03548
PhilFortunaJr <philfortunajr@gmail.com>
2019-03-22 21:00:15 -0400

    6.6.2

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_005458) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "year"
-    t.integer "price"
+    t.decimal "price"
   end
 
   create_table "users", force: :cascade do |t|
